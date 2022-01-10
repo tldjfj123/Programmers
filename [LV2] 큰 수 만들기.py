bbf947e42f97478ab6a7385325b3ffbc5eefe5c7
tldjfj123 <dksckd0804@naver.com>
2022-01-10 13:47:31 +0900

    22.01.10

---
+++ [LV2] 큰 수 만들기.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def solution(number, k) :
+    stack = [number[0]]
+    
+    for i in number[1 : ] :
+        while len(stack) > 0 and stack[-1] < i and k > 0 :
+            k -= 1 
+            stack.pop()
+        stack.append(i)
+    
+    if k != 0 :
+        stack = stack[: -k]
+    
+    return ''.join(stack)
